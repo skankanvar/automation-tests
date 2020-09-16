51c3703841db3f0352cde4ab4089a1e747aa8f6b
Sai Sabarish Kankanvar <sabarish@sais-mbp.myfiosgateway.com>
2020-09-16 19:41:44 -0400

    adding hcanges

--- src/main/java/configs/ConfigManager.java
+++ src/main/java/configs/ConfigManager.java
@@ -7,22 +7,37 @@ import java.util.Properties;
 
 public class ConfigManager {
 
-	public static String GetProperty(String propName) {
-		File configFile = new File("config.properties");
+	public static String GetProperty(String propName){
 		String propValue = "";
 		try {
-		    FileReader reader = new FileReader(configFile);
-		    Properties props = new Properties();
-		    props.load(reader);
-		 
-		    propValue = props.getProperty(propName);
-		 
-		    System.out.println("PropValue: " + propValue);
-		    reader.close();
-		    
-		    return  propValue;
+			java.io.InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("properties-from-pom.properties");
+			java.util.Properties properties = new Properties();
+			properties.load(inputStream);
+			String  env = properties.getProperty("env");
+
+			File configFile = null;
+			if(env.equalsIgnoreCase("local"))
+			{
+				System.out.println("local props");
+				configFile = new File("config.local.properties");
+			}
+			else {
+				System.out.println("server props");
+				configFile = new File("config.properties");
+			}
+
+			FileReader reader = new FileReader(configFile);
+			Properties props = new Properties();
+			props.load(reader);
+
+			propValue = props.getProperty(propName);
+
+			System.out.println("PropValue: " + propValue);
+			reader.close();
+
+			return  propValue;
 		} catch (FileNotFoundException ex) {
-		    System.out.println(" file does not exist");
+			System.out.println(" file does not exist");
 		} catch (IOException ex) {
 			System.out.println(" io exception");
 		}
